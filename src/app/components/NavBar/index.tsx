985563c26787b784081aceefad42af616eaf5cdd
rodrigo_motocasystems <rodrigo@motocasystems.com.br>
2023-11-23 17:42:05 -0300

    iniciando criação do menu de navegação

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -2,16 +2,17 @@ import Image from 'next/image';
 
 export default function NavBar() {
 	return (
-		<nav className='flex flex-row justify-between bg-white w-3/4 rounded-b-3xl shadow-md p-8 '>
+		<nav className='flex flex-row justify-between bg-white w-3/4 rounded-b-3xl shadow-md px-8 py-6'>
 			<Image src='logo.svg' alt='Logo do sindicato' width={90} height={90} />
 
-			<div>
-				<span>testess</span>
-				<span>testess</span>
+			<div className='w-2/4 flex justify-between items-center text-sm'>
+				<span>Início</span>
+				<span>Institucional</span>
+				<span>Convenções</span>
+				<span>Serviços</span>
 			</div>
 
 			<div>
-				<span>testess</span>
 				<span>testess</span>
 			</div>
 		</nav>
