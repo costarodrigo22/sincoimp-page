d40e28a04588c825fa89f2cc0956a33088d8fe02
rodrigo_motocasystems <rodrigo@motocasystems.com.br>
2023-11-27 15:56:42 -0300

    Criação do título da segunda sessão

--- src/components/BenefitsSession/index.tsx
+++ src/components/BenefitsSession/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function BenefitsSession() {
 	return (
 		<div className='w-full h-2/3 flex justify-center lg:px-3'>
-			<div className='w-full h-full bg-slate-100 flex lg:w-3/4'>
+			<div className='w-full h-full bg-slate-100 flex lg:w-4/5'>
 				<div className='hidden lg:w-1/2 lg:flex'>
 					<Image
 						src='image-default.svg'
@@ -14,7 +14,17 @@ export default function BenefitsSession() {
 					/>
 				</div>
 
-				<div className='w-full lg:w-1/2'>
+				<div className='w-full flex flex-col items-center pt-8 lg:w-1/2'>
+					<span className='font-medium text-3xl'>
+						Por que fialiar -se ao <strong>sincoimp?</strong>
+					</span>
+
+					<span>
+						Tenha seus direitos garantidos além de diversos beneficios.
+					</span>
+					<p>textos</p>
+					<p>textos</p>
+					<p>textos</p>
 					<p>textos</p>
 				</div>
 			</div>
