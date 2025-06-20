656e1c9faf45b74b2e47864466bd4384b8493a52
hiagoborgaco <hiagoborgaco@motoca.com.br>
2025-06-20 11:21:44 -0300

    Passando a usar o nome do site e da descrição através da variável de ambiente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,42 +3,48 @@ import { Inter, Poppins } from 'next/font/google';
 import './globals.css';
 
 const inter = Poppins({
-	weight: [
-		'100',
-		'200',
-		'300',
-		'400',
-		'500',
-		'600',
-		'700',
-		'800',
-		'900',
-		'100',
-		'200',
-		'300',
-		'400',
-		'500',
-		'600',
-		'700',
-		'800',
-		'900',
-	],
-	subsets: ['latin'],
+  weight: [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+  ],
+  subsets: ['latin'],
 });
 
+const theme = process.env.CLIENT_NAME || 'default';
+const iconPath = `/clients/${theme}/favicon.svg`;
+
 export const metadata: Metadata = {
-	title: 'Sincoimp',
-	description: 'Sindicato Imperatriz',
+  title: process.env.PAGE_TITLE,
+  description: process.env.PAGE_DESCRIPTION,
+  icons: {
+    icon: iconPath,
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
