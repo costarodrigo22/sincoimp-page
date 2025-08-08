226d51510c75f799e2935e1ed66c23d3a8d8aaf6
rodrigo_motocasystems <rodrigocosta@motoca.com.br>
2025-08-08 17:21:30 -0300

    adicionando redirecionamento para whatsApp só no sincogra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,33 +4,33 @@ import './globals.css';
 import { useEnvStore } from './contexts/EnvContext';
 
 const inter = Poppins({
-  weight: [
-    '100',
-    '200',
-    '300',
-    '400',
-    '500',
-    '600',
-    '700',
-    '800',
-    '900',
-    '100',
-    '200',
-    '300',
-    '400',
-    '500',
-    '600',
-    '700',
-    '800',
-    '900',
-  ],
-  subsets: ['latin'],
+	weight: [
+		'100',
+		'200',
+		'300',
+		'400',
+		'500',
+		'600',
+		'700',
+		'800',
+		'900',
+		'100',
+		'200',
+		'300',
+		'400',
+		'500',
+		'600',
+		'700',
+		'800',
+		'900',
+	],
+	subsets: ['latin'],
 });
 
 useEnvStore.setState({
-  pageTitle: process.env.PAGE_TITLE,
-  pageDescription: process.env.PAGE_DESCRIPTION,
-  clientName: process.env.CLIENT_NAME,
+	pageTitle: process.env.PAGE_TITLE,
+	pageDescription: process.env.PAGE_DESCRIPTION,
+	clientName: process.env.NEXT_PUBLIC_CLIENT_NAME,
 });
 
 const theme = useEnvStore.getState().clientName;
@@ -38,21 +38,21 @@ const theme = useEnvStore.getState().clientName;
 const iconPath = `/clients/${theme}/favicon.svg`;
 
 export const metadata: Metadata = {
-  title: useEnvStore.getState().pageTitle,
-  description: useEnvStore.getState().pageDescription,
-  icons: {
-    icon: iconPath,
-  },
+	title: useEnvStore.getState().pageTitle,
+	description: useEnvStore.getState().pageDescription,
+	icons: {
+		icon: iconPath,
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='pt-BR'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -9,75 +9,90 @@ import { httpClient } from '@/utils/httpClient';
 import { cn } from '@/utils/Cn';
 
 export default function NavBar() {
-  const [openMenuResponsive, setOpenMenuResponsive] = useState(false);
-  const [base64, setBase64] = useState<string>('');
-  const [openBankslipModal, setOpenBankslipModal] = useState(false);
-  const { setOpenReportModal } = useReportModal();
+	const [openMenuResponsive, setOpenMenuResponsive] = useState(false);
+	const [base64, setBase64] = useState<string>('');
+	const [openBankslipModal, setOpenBankslipModal] = useState(false);
+	const [clientName, setClientName] = useState<string | null>(null);
+	const { setOpenReportModal } = useReportModal();
 
-  async function handleGetData() {
-    try {
-      const { data } = await httpClient.get('/api/without/empresa/logo');
-      setBase64(data.data[0].base64);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	const whatsappNumber = '559991558807';
 
-  useEffect(() => {
-    handleGetData();
-  }, []);
+	// const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
-  function handleMenuResponsive() {
-    setOpenMenuResponsive(!openMenuResponsive);
-  }
+	async function handleGetData() {
+		try {
+			const { data } = await httpClient.get('/api/without/empresa/logo');
+			setBase64(data.data[0].base64);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  const handleScroll = (id: string) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+	useEffect(() => {
+		handleGetData();
+	}, []);
 
-  const clientName = process.env.CLIENT_NAME;
+	useEffect(() => {
+		// Garante que a leitura só acontece no cliente
+		setClientName(process.env.NEXT_PUBLIC_CLIENT_NAME || '');
+	}, []);
 
-  return (
-    <>
-      <BankslipModal
-        isOpen={openBankslipModal}
-        setIsOpen={setOpenBankslipModal}
-      />
-      <nav className="z-30 absolute hidden flex-row justify-between items-center bg-white w-full rounded-b-3xl shadow-md px-12 py-6 md:flex lg:w-3/4">
-        {base64 && (
-          <Image src={base64} alt="Logo do sindicato" width={75} height={81} />
-        )}
+	if (clientName === null) {
+		// Evita renderização até termos o valor, prevenindo hidratação incorreta
+		return null;
+	}
 
-        <div className="w-2/4 flex justify-between items-center text-sm font-medium">
-          <div
-            className=" p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer"
-            onClick={() => handleScroll('benefitsSection')}
-          >
-            <a className="cursor-pointer font-medium">Benefícios</a>
-          </div>
-          <div
-            className=" p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer"
-            onClick={() => handleScroll('actionsSection')}
-          >
-            <a className="cursor-pointer font-medium">Ações</a>
-          </div>
-          <div
-            className=" p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer"
-            onClick={() => handleScroll('conventionsSection')}
-          >
-            <a className="cursor-pointer font-medium">Convenções</a>
-          </div>
-          <div
-            className=" p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer"
-            onClick={() => handleScroll('doubtsSection')}
-          >
-            <a className="cursor-pointer font-medium">Dúvidas</a>
-          </div>
+	function handleMenuResponsive() {
+		setOpenMenuResponsive(!openMenuResponsive);
+	}
 
-          {/* <div
+	const handleScroll = (id: string) => {
+		const section = document.getElementById(id);
+		if (section) {
+			section.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
+
+	// const clientName = process.env.CLIENT_NAME;
+
+	return (
+		<>
+			<BankslipModal
+				isOpen={openBankslipModal}
+				setIsOpen={setOpenBankslipModal}
+			/>
+			<nav className='z-30 absolute hidden flex-row justify-between items-center bg-white w-full rounded-b-3xl shadow-md px-12 py-6 md:flex lg:w-3/4'>
+				{base64 && (
+					<Image src={base64} alt='Logo do sindicato' width={75} height={81} />
+				)}
+
+				<div className='w-2/4 flex justify-between items-center text-sm font-medium'>
+					<div
+						className=' p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer'
+						onClick={() => handleScroll('benefitsSection')}
+					>
+						<a className='cursor-pointer font-medium'>Benefícios</a>
+					</div>
+					<div
+						className=' p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer'
+						onClick={() => handleScroll('actionsSection')}
+					>
+						<a className='cursor-pointer font-medium'>Ações</a>
+					</div>
+					<div
+						className=' p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer'
+						onClick={() => handleScroll('conventionsSection')}
+					>
+						<a className='cursor-pointer font-medium'>Convenções</a>
+					</div>
+					<div
+						className=' p-2 rounded-[10px] hover:bg-slate-200 transition-colors cursor-pointer'
+						onClick={() => handleScroll('doubtsSection')}
+					>
+						<a className='cursor-pointer font-medium'>Dúvidas</a>
+					</div>
+
+					{/* <div
             onClick={() => {
               setOpenReportModal(true);
             }}
@@ -92,125 +107,134 @@ export default function NavBar() {
             />
             <span className="font-medium">Denuncie aqui</span>
           </div> */}
-          {/* <span>Início</span>
+					{/* <span>Início</span>
 					<span>Institucional</span>
 					<span>Convenções</span>
 					<span>Serviços</span> */}
-        </div>
+				</div>
 
-        <div
-          onClick={() => {
-            setOpenBankslipModal(true);
-          }}
-          className={cn(
-            'flex items-center w-40 h-[50px] justify-center border-2 border-black rounded-3xl cursor-pointer hover:shadow-lg transition duration-300 ease-in-out',
-            clientName === 'sincogra'
-              ? 'pointer-events-none bg-slate-300 opacity-25'
-              : ''
-          )}
-        >
-          <span className="font-medium">Emita seu boleto</span>
-        </div>
-      </nav>
+				<div
+					onClick={() => {
+						if (clientName === 'sincogra') {
+							// const whatsappNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER; // ex: "5511999999999"
+							if (whatsappNumber) {
+								window.open(`https://wa.me/${whatsappNumber}`, '_blank');
+							} else {
+								console.error('Número do WhatsApp não definido no .env');
+							}
+						} else {
+							setOpenBankslipModal(true);
+						}
+					}}
+					className={cn(
+						'flex items-center w-40 h-[50px] justify-center border-2 border-black rounded-3xl cursor-pointer hover:shadow-lg transition duration-300 ease-in-out',
+						clientName === 'sincogra' ? 'bg-[#027cff8e]' : ''
+					)}
+				>
+					<span className='font-medium'>
+						{clientName === 'sincogra' ? 'Filiar-se' : 'Emita seu boleto'}
+					</span>
+				</div>
+			</nav>
 
-      <nav className="flex flex-col w-full fixed md:hidden z-10">
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="absolute top-4 left-4 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-          onClick={handleMenuResponsive}
-        >
-          {openMenuResponsive && (
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-          )}
-          {!openMenuResponsive && (
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          )}
-        </button>
+			<nav className='flex flex-col w-full fixed md:hidden z-10'>
+				<button
+					data-collapse-toggle='navbar-default'
+					type='button'
+					className='absolute top-4 left-4 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+					aria-controls='navbar-default'
+					aria-expanded='false'
+					onClick={handleMenuResponsive}
+				>
+					{openMenuResponsive && (
+						<svg
+							className='w-6 h-6 text-gray-800 dark:text-white'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 14 14'
+						>
+							<path
+								stroke='currentColor'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
+							/>
+						</svg>
+					)}
+					{!openMenuResponsive && (
+						<svg
+							className='w-5 h-5'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 17 14'
+						>
+							<path
+								stroke='currentColor'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M1 1h15M1 7h15M1 13h15'
+							/>
+						</svg>
+					)}
+				</button>
 
-        {openMenuResponsive && (
-          <div className="flex flex-col h-screen px-4 py-16 bg-slate-50 gap-6 w-[300px] shadow-lg">
-            {/* <span className="cursor-pointer font-medium mt-4">Início</span>
+				{openMenuResponsive && (
+					<div className='flex flex-col h-screen px-4 py-16 bg-slate-50 gap-6 w-[300px] shadow-lg'>
+						{/* <span className="cursor-pointer font-medium mt-4">Início</span>
             <span className="cursor-pointer font-medium">Institucional</span>
             <span className="cursor-pointer font-medium">Convenções</span>
             <span className="cursor-pointer font-medium">Serviços</span>
             <span className="cursor-pointer font-medium">Emita seu boleto</span> */}
-            <a
-              className="cursor-pointer font-medium"
-              onClick={() => handleScroll('benefitsSection')}
-            >
-              Benefícios
-            </a>
-            <a
-              className="cursor-pointer font-medium"
-              onClick={() => handleScroll('actionsSection')}
-            >
-              Ações
-            </a>
-            <a
-              className="cursor-pointer font-medium"
-              onClick={() => handleScroll('doubtsSection')}
-            >
-              Dúvidas
-            </a>
-            <a
-              className="cursor-pointer font-medium"
-              onClick={() => handleScroll('conventionsSection')}
-            >
-              Convenções
-            </a>
-            <a
-              className="cursor-pointer font-medium"
-              onClick={() => handleScroll('uKnowSection')}
-            >
-              Você sabia?
-            </a>
-          </div>
-        )}
-      </nav>
-      <div className=" fixed bottom-5 z-40 right-5">
-        <div
-          className=" bg-[#007bff] rounded-full p-3 cursor-pointer text-lg shadow-md shadow-[rgba(0, 0, 0, 0.1)]"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        >
-          <Image
-            width={30}
-            height={30}
-            src="arrow-upIcon.svg"
-            alt="Voltar para o topo"
-          />
-        </div>
-      </div>
-    </>
-  );
+						<a
+							className='cursor-pointer font-medium'
+							onClick={() => handleScroll('benefitsSection')}
+						>
+							Benefícios
+						</a>
+						<a
+							className='cursor-pointer font-medium'
+							onClick={() => handleScroll('actionsSection')}
+						>
+							Ações
+						</a>
+						<a
+							className='cursor-pointer font-medium'
+							onClick={() => handleScroll('doubtsSection')}
+						>
+							Dúvidas
+						</a>
+						<a
+							className='cursor-pointer font-medium'
+							onClick={() => handleScroll('conventionsSection')}
+						>
+							Convenções
+						</a>
+						<a
+							className='cursor-pointer font-medium'
+							onClick={() => handleScroll('uKnowSection')}
+						>
+							Você sabia?
+						</a>
+					</div>
+				)}
+			</nav>
+			<div className=' fixed bottom-5 z-40 right-5'>
+				<div
+					className=' bg-[#007bff] rounded-full p-3 cursor-pointer text-lg shadow-md shadow-[rgba(0, 0, 0, 0.1)]'
+					onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+				>
+					<Image
+						width={30}
+						height={30}
+						src='arrow-upIcon.svg'
+						alt='Voltar para o topo'
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
